d70139f6cc3a3bf01313b9175e7e07da684787ca
John Woodell <woodie@netpress.com>
2019-01-22 19:49:18 -0800

    Cleanup UI and animations.

--- src/UberMe.java
+++ src/UberMe.java
@@ -23,11 +23,13 @@ public class UberMe extends MIDlet
   private final int padding = 4;
   private boolean painting = false;
   private static Image pinImage = null;
-  private static Image currentImage = null;
-  private static Image splashImage = null;
+  private static Image mapImage = null;
+  private static Image knockoutImage= null;
+  private static Image backgroundImage = null;
+  private static Image logoImage = null;
+  private static Image brandingImage = null;
   private static Image addressImage = null;
-  private static Image requestImage = null;
-  private static Image requestingImage = null;
+  private static Image big2minImage = null;
   private static Image enrouteImage = null;
   private static Image openSansBold = null;
   private static Image openSansLight = null;
@@ -72,10 +74,9 @@ public class UberMe extends MIDlet
     protected Timer timer;
     protected TimerTask updateTask;
     static final int FRAME_DELAY = 40;
-    private int circle_w = 170;
-    private int circle_min = 160;
-    private int circle_max = 180;
-    private int circle_pos = 190;
+    private int circle_min = 170;
+    private int circle_pos = 185;
+    private int circle_max = 200;
     private int circle_direction = 1;
     private int progress_w = padding;
 
@@ -86,10 +87,12 @@ public class UberMe extends MIDlet
       height = getHeight();
       try {
         pinImage = Image.createImage ("/pin.png");
-        splashImage = Image.createImage ("/splash.png");
+        mapImage = Image.createImage ("/map.png");
+        knockoutImage = Image.createImage ("/knockout.png");
+        logoImage = Image.createImage ("/icon72x72.png");
+        brandingImage = Image.createImage ("/branding.png");
         addressImage = Image.createImage ("/address.png");
-        requestImage = Image.createImage ("/request.png");
-        requestingImage = Image.createImage ("/requesting.png");
+        big2minImage = Image.createImage ("/big2min.png");
         enrouteImage = Image.createImage ("/enroute.png");
         openSansBold = Image.createImage ("/sans-bold-20.png");
         openSansLight = Image.createImage ("/sans-light-20.png");
@@ -119,7 +122,6 @@ public class UberMe extends MIDlet
           } else if (state == 2) {
             showProgress();
           }
-          //repaint clock
           repaint(width - 100, 0, 100, 20);
         }
       };
@@ -132,13 +134,13 @@ public class UberMe extends MIDlet
     }
 
     public synchronized void pulseCircle() {
-      circle_w += (1 * circle_direction);
-      if (circle_w <= circle_min) {
+      circle_pos += (1 * circle_direction);
+      if (circle_pos <= circle_min) {
         circle_direction = 1;
-      } else if (circle_w >= circle_max) {
+      } else if (circle_pos >= circle_max) {
         circle_direction = -1;
       }
-      repaint(15, 90, 200, 200);
+      repaint((width - circle_max) / 2, (height - circle_max) / 2, circle_max, circle_max);
     }
 
     public synchronized void showProgress() {
@@ -180,44 +182,56 @@ public class UberMe extends MIDlet
       int minute = calendar.get(Calendar.MINUTE);
       int second = calendar.get(Calendar.SECOND);
       String strTime = "" + hour + (minute < 10 ? ":0" : ":") + minute;
-      Font fontSm = Font.getFont(Font.FACE_SYSTEM, Font.STYLE_PLAIN, Font.SIZE_MEDIUM);
-      Font fontLg = Font.getFont(Font.FACE_SYSTEM, Font.STYLE_BOLD, Font.SIZE_LARGE);
-
+      // Load some page defaults
       if (state == -1) {
-        currentImage = splashImage;
+        backgroundImage = null;
         strText = "";
         strTime = "";
       } else if (state == 0) {
-        currentImage = addressImage;
+        backgroundImage = addressImage;
         strText = "Uber Me";
       } else if (state == 1) {
-        currentImage = requestImage;
-        strText = "Request";
+        backgroundImage = null;
+        strText = "UberX";
       } else if (state == 2) {
-        currentImage = requestingImage;
-        strText = "Requesting...";
+        backgroundImage = mapImage;
+        strText = "Requesting";
       } else {
-        currentImage = enrouteImage;
+        backgroundImage = enrouteImage;
         strText = "En Route";
       }
-      g.drawImage(currentImage, 0, 0, Graphics.LEFT | Graphics.TOP);
-      g.setColor(0xFFFFFF);
-      letterFont = openSansBold;
-      letters(g, strText, 4, 4);
-      letters(g, strTime, width - (strTime.length() * 12) + 2, 4);
-      g.setFont(fontSm);
-      if (state == 1) {
-        int cx = width / 2 - (circle_w / 2);
-        int cy = circle_pos - (circle_w / 2);
+      // Set a background image or just fill black
+      if (null == backgroundImage) {
+        g.setColor(0x000000);
+        g.fillRect(0, 0, width, height);
+      } else {
+        g.drawImage(backgroundImage, width / 2, height / 2, Graphics.HCENTER | Graphics.VCENTER);
+      }
+
+      Font fontSm = Font.getFont(Font.FACE_SYSTEM, Font.STYLE_PLAIN, Font.SIZE_MEDIUM);
+      Font fontLg = Font.getFont(Font.FACE_SYSTEM, Font.STYLE_BOLD, Font.SIZE_LARGE);
+      // Place page specific elements
+      if (state == -1) {
+        g.drawImage(logoImage, width / 2, height / 2 - 50, Graphics.VCENTER | Graphics.HCENTER);
+        g.drawImage(brandingImage, width / 2, height / 2 + 50, Graphics.VCENTER | Graphics.HCENTER);
+      } else if (state == 1) {
+        int cx = width / 2 - (circle_pos / 2);
+        int cy = height / 2 - (circle_pos / 2);
+        int w = 4;
         g.setColor(0x24c4e2);
-        for (int x = 0; x < 4; x++) {
-          g.drawArc(cx + x, cy + x, circle_w - (2 * x), circle_w - (2 * x), 0, 365);
-        }
+        g.fillArc(cx + 0, cy + 0, circle_pos - (2 * 0), circle_pos - (2 * 0), 0, 365);
+        g.setColor(0x000000);
+        g.fillArc(cx + w, cy + w, circle_pos - (2 * w), circle_pos - (2 * w), 0, 365);
         g.setColor(0xFFFFFF);
         letters(g, "REQUEST", width / 2 - 44, height - 23);
         g.setColor(0x99FF99);
         g.drawRoundRect(width / 2 - 52, height - 28, 104, 27, 9, 9);
+        g.drawImage(big2minImage, width / 2, height / 2, Graphics.VCENTER | Graphics.HCENTER);
       } else if (state == 2) {
+        g.drawImage(knockoutImage, 0, 0, Graphics.LEFT | Graphics.TOP);
+        g.setColor(0x000000);
+        g.drawArc(width / 2 - 100 + 3, height / 2 - 100 + 3, circle_max - 6, circle_max - 6, 0, 365);
+        g.drawArc(width / 2 - 100 + 4, height / 2 - 100 + 4, circle_max - 8, circle_max - 8, 0, 365);
         g.setColor(0xFFFFFF);
         letters(g, "CANCEL", width / 2 - 38, height - 23);
         g.setColor(0xFF3333);
@@ -225,7 +239,7 @@ public class UberMe extends MIDlet
         g.setColor(0x99FF99);
         g.setStrokeStyle(Graphics.DOTTED);
         g.drawRect(padding, 32, progress_w, 1);
-        g.drawImage(pinImage, width / 2, height / 2, Graphics.HCENTER | Graphics.VCENTER);
+        g.drawImage(pinImage, width / 2, height / 2, Graphics.HCENTER | Graphics.BOTTOM);
       }
       // For the enroute page, the timer has a 74px with black filled circle
       // with a 64px wide blue stroked circle. There is a 5px wide blue circle
@@ -233,11 +247,16 @@ public class UberMe extends MIDlet
 
       // No menus on Splash Screen or Requesting Page (which shoud be a modal dialog)
       if (state != -1 && state != 2) {
+        g.setFont(fontSm);
         g.setColor(0xFFFFFF);
         g.drawString("Menu", padding, height - fontSm.getHeight(), Graphics.LEFT | Graphics.TOP);
         g.drawString("Back", width - padding - fontSm.stringWidth("Back"),
                                 height - fontSm.getHeight(), Graphics.LEFT | Graphics.TOP);
       }
+      g.setColor(0xFFFFFF);
+      letterFont = openSansBold;
+      letters(g, strText, 4, 4);
+      letters(g, strTime, width - (strTime.length() * 12) + 2, 4);
       painting = false;
     }
 
